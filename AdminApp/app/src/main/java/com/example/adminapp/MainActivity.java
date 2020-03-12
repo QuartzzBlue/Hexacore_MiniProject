0fc50513b2e231d5072fe2347205f97d295d8acd
Seul <seulee95@naver.com>
2020-03-12 14:48:07 +0900

    add AdminApp directory 200312

---
+++ AdminApp/app/src/main/java/com/example/adminapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.adminapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText tip,control;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        tip = findViewById(R.id.tip);
+        control = findViewById(R.id.control);
+        button = findViewById(R.id.button);
+
+    }
+
+    public void ckbt(View v){
+        if(v.getId()==R.id.button){
+            new SendServer(tip.getText().toString(),control.getText().toString()).start();
+        }
+    }
+
+
+    class SendServer extends Thread {
+
+        String urlstr = "http://70.12.113.200/Network_Web/webapp";
+
+        public SendServer(String id, String txt) {
+            urlstr += "?ip=" + id + "&txt=" + txt;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(urlstr);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.getInputStream();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
---
+++ AdminApp/app/src/main/java/com/example/adminapp/MyFirebaseMessagingService.java
@@ -0,0 +1,56 @@
+package com.example.adminapp;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d("===", "Refreshed token: " + token);
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("===", "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d("===", msg);
+                        Toast.makeText(MyFirebaseMessagingService.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+    public static String getToken(Context context) {
+        return context.getSharedPreferences("_", MODE_PRIVATE).getString("fb", "empty");
+    }
+
+
+}
